f4d48564b7e2bf2e20740572cea157446c338a40
pctronique <pctronique@free.fr>
2024-05-14 14:02:29 +0200

    modif clear

--- .docker/bin/clear.sh
+++ .docker/bin/clear.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo service openvpn stop
-docker network prune -f
+sudo docker network prune -f
 sudo service openvpn stop
 
-docker stop $(docker ps -aq) && sudo docker rm $(docker ps -aq) && sudo docker rmi -f $(docker images -q)
+sudo docker stop $(docker ps -aq) && sudo docker rm $(docker ps -aq) && sudo docker rmi -f $(docker images -q)
+sudo service docker stop
+sudo rm -r /var/lib/docker/overlay2
+sudo service docker start
+sudo docker system prune --volumes -a
